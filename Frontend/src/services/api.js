c81b6ae6f15affc5c39c3a021e0e16670d2d1a8b
SimonCastroV <secastrov@eafit.edu.co>
2026-09-04 13:15:44 -0500

    estructura inicial: backend Django + frontend Vue3

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Adjunta el token de autenticacion en cada peticion, si existe
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Token ${token}`
+  }
+  return config
+})
+
+export default api
